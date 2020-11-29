586f23cf0bbaf80e8e70277442c4e0de7e7612f5
Jim McCann <ix@tchow.com>
2020-11-29 15:56:09 -0500

    <string> header was missing

--- MeshBuffer.hpp
+++ MeshBuffer.hpp
@@ -2,6 +2,7 @@
 
 #include "GL.hpp"
 #include <map>
+#include <string>
 
 //"MeshBuffer" holds a collection of meshes loaded from a file
 // (note that meshes in a single collection will share a vbo/vao)
